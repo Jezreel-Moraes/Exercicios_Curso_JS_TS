b85ec2b2a5c519799afab4c5c8bfe784bfa2bfdd
Jezreel-Moraes <jezreel.aluno@gmail.com>
2022-06-24 10:35:13 -0300

    refactored clock

--- ex - cronometro/assets/js/main.js
+++ ex - cronometro/assets/js/main.js
@@ -1,59 +1,96 @@
-const clock = document.querySelector("#clock");
-const clockStart = document.querySelector("#start");
-const clockStop = document.querySelector("#stop");
-const clockReset = document.querySelector("#reset");
+function clock() {
+  const clock = document.querySelector("#clock");
 
-clockStart.addEventListener("click", startClock);
-clockStop.addEventListener("click", stopClock);
-clockReset.addEventListener("click", resetClock);
+  document.addEventListener("click", function (event) {
+    const element = event.target;
+    if (!element.hasAttribute("id")) return;
 
-let timer;
-let [hours, minutes, seconds] = [0, 0, 0];
+    const attribute = element.getAttribute("id");
 
-function startClock() {
-  clock.className = "";
-  clearInterval(timer);
-  timer = setInterval(updateClock, 1000);
-}
+    const buttonFunctions = {
+      start: startClock,
+      stop: stopClock,
+      reset: resetClock,
+    };
 
-const stopTimer = () => clearInterval(timer);
-const addZero = (number) => {
-  return number < 10 ? `0${number}` : number;
-};
-const printClock = () => {
-  [h, m, s] = [hours, minutes, seconds].map(addZero);
-  clock.innerText = `${h}:${m}:${s}`;
-};
+    const buttonFunction = buttonFunctions[attribute];
+    return buttonFunction ? buttonFunction() : null;
+  });
 
-function stopClock() {
-  clock.className = "stopped";
-  stopTimer();
-}
+  // let [hours, minutes, seconds, milliseconds] = [0, 0, 0, 0];
+  let timer;
+  let milliseconds = 0;
+  const timeOut = 10;
 
-const resetTimer = () => ([hours, minutes, seconds] = [0, 0, 0]);
-
-function resetClock() {
-  clock.className = "";
-  stopTimer();
-  resetTimer();
-  printClock();
-}
-
-const incrementTime = () => {
-  seconds++;
-
-  if (seconds >= 60) {
-    seconds = 0;
-    minutes++;
+  function startClock() {
+    clock.className = "";
+    clearInterval(timer);
+    timer = setInterval(updateClock, timeOut);
   }
-  if (minutes >= 60) {
-    minutes = 0;
-    hours++;
-  }
-  if (hours >= 24) resetTimer();
-};
 
-const updateClock = () => {
-  incrementTime();
-  printClock();
-};
+  const stopTimer = () => clearInterval(timer);
+
+  const addZero = (number) => {
+    return number < 10 ? `0${number}` : number;
+  };
+
+  const createClockWithMilliseconds = (milliseconds) => {
+    const date = new Date(milliseconds * timeOut);
+    const dateMillisecondsDividedByTimeOut = date.getMilliseconds() / timeOut;
+    const clockTime =
+      date
+        .toLocaleTimeString("pt-BR", {
+          hour12: false,
+          timeZone: "UTC",
+        })
+        .toString() + `:${addZero(dateMillisecondsDividedByTimeOut)}`;
+
+    return clockTime;
+  };
+
+  const printClock = () => {
+    // [h, m, s, ms] = [hours, minutes, seconds, milliseconds].map(addZero);
+    // clock.innerText = `${h}:${m}:${s}:${ms}`;
+    clock.innerText = createClockWithMilliseconds(milliseconds);
+  };
+
+  function stopClock() {
+    clock.className = "stopped";
+    stopTimer();
+  }
+
+  const resetTimer = () =>
+    // ([hours, minutes, seconds, milliseconds] = [0, 0, 0, 0]);
+    (milliseconds = 0);
+
+  function resetClock() {
+    clock.className = "";
+    stopTimer();
+    resetTimer();
+    printClock();
+  }
+
+  const incrementTime = () => {
+    milliseconds++;
+    // if (milliseconds >= 100) {
+    //   milliseconds = 0;
+    //   seconds++;
+    // }
+    // if (seconds >= 60) {
+    //   seconds = 0;
+    //   minutes++;
+    // }
+    // if (minutes >= 60) {
+    //   minutes = 0;
+    //   hours++;
+    // }
+    // if (hours >= 24) resetTimer();
+  };
+
+  const updateClock = () => {
+    incrementTime();
+    printClock();
+  };
+}
+
+clock();
