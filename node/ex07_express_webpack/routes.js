cc27b2f743a4dd3b62d959aa5aad61b4af2c221c
Jezreel Kintino de Moraes <jezreel.aluno@gmail.com>
2022-07-01 20:27:16 -0300

    express + webpack

---
+++ node/ex07_express_webpack/routes.js
@@ -0,0 +1,9 @@
+const express = require("express");
+const route = express.Router();
+
+// home
+
+const homeController = require("./src/controllers/homeController");
+route.get("/", homeController);
+
+module.exports = route;
