93835eebcb2b9f245845ede857cf72deb51401b0
Jezreel Kintino de Moraes <jezreel.aluno@gmail.com>
2022-07-03 09:26:56 -0300

    finish node express

--- node/ex11_helmet_CSFR/routes.js
+++ node/ex11_helmet_CSFR/routes.js
@@ -2,7 +2,14 @@ const express = require("express");
 const route = express.Router();
 
 // home
+
 const homeController = require("./src/controllers/homeController");
 route.get("/", homeController);
+
+//form
+
+const formController = require("./src/controllers/formController");
+route.get("/form", formController.get);
+route.post("/form", formController.post);
 
 module.exports = route;
--- node/ex11_helmet_CSFR/server.js
+++ node/ex11_helmet_CSFR/server.js
@@ -2,13 +2,19 @@ require("dotenv").config();
 
 const express = require("express");
 const app = express();
-const someMiddleware = require("./src/middlewares/someMiddleware");
+const {
+  someMiddleware,
+  checkCsrfError,
+  csrfMiddleware,
+} = require("./src/middlewares/middleware");
 const routes = require("./routes");
 const path = require("path");
 const mongoose = require("mongoose");
 const session = require("express-session");
 const MongoStore = require("connect-mongo");
 const flash = require("connect-flash");
+const helmet = require("helmet");
+const csrf = require("csurf");
 
 mongoose
   .connect(process.env.CONNECTION_STRING, {
@@ -34,10 +40,16 @@ const sessionOptions = session({
   },
 });
 
+app.use(helmet());
 app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 app.use(express.static(path.resolve(__dirname, "public")));
 app.use(sessionOptions);
 app.use(flash());
+
+app.use(csrf());
+app.use(checkCsrfError);
+app.use(csrfMiddleware);
 app.use(someMiddleware);
 app.use(routes);
 // app.use('/route', middleware)
